d721c8e73283385556f38fe66488ab3f932673ef
Diacryptic <Diacryptic@users.noreply.github.com>
2020-02-27 10:46:50 +0000

    Update crawl.py
    wrote the algorithm

--- crawl.py
+++ crawl.py
@@ -1,37 +1,11 @@
 import pages
 
-#Example:
-start = pages.Page("https://www.whsb.essex.sch.uk")
-print("start:    ", start)
-print("    start.url:    ", start.url)
-print("    start.loaded:    ", start.loaded)
-print("    start.source:    ", start.source)
-print("    start.content:    ", start.content)
-print("    start.links:    ", start.links)
-
-print("\nstart.load()\n")
-start.load()
-
-print("start:    ", start)
-print("    start.url:    ", start.url)
-print("    start.loaded:    ", start.loaded)
-print("    start.source:    ", start.source)
-print("    start.content:    ", start.content)
-print("    start.links:    ", start.links)
-
-#End example
-import urllib.parse
-
-webpages = {}
-start = pages.Page("https://www.whsb.essex.sch.uk")
-webpages[start.url] = start
-while len(webpages) < 200:
-    print("crawling")
-    print(len(webpages))
-    for url in list(webpages.keys()):
-        page = webpages[url]
-        if not page.loaded:
-            page.load()
-        for link in page.links:
-            if not link in webpages.keys():
-                webpages[link] = pages.Page(link)
+crawled = []
+to_be_crawled = ["https://www.whsb.essex.sch.uk"]
+while len(crawled) < 10:
+    start = pages.Page(to_be_crawled[0])
+    start.load()
+    to_be_crawled += start.links
+    to_be_crawled = list(dict.fromkeys(to_be_crawled))
+    crawled.append(start)
+    to_be_crawled.pop(0)
