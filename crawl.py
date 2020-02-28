5b83c23f8d206188d190f91053a9b56f46e3789a
Diacryptic <Diacryptic@users.noreply.github.com>
2020-02-28 15:18:17 +0000

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,13 +1,14 @@
 import pages
+from random import randint
 
-def crawl(website):
+def crawl(website, depth = 10):
     crawled = []
     to_be_crawled = [website]
-    while len(crawled) < 10:
-        start = pages.Page(to_be_crawled[0])
+    while len(crawled) < depth:
+        x = randint(0, len(to_be_crawled))
+        start = pages.Page(to_be_crawled.pop(x))
         start.load()
         to_be_crawled += start.links
         to_be_crawled = list(dict.fromkeys(to_be_crawled))
         crawled.append(start)
-        to_be_crawled.pop(0)
     return (crawled)
