9b45dae6005e34437ac6d7989dc960519504ff48
Diacryptic <Diacryptic@users.noreply.github.com>
2020-02-28 14:56:30 +0000

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,14 +1,14 @@
 import pages
-class Crawl:
-    def crawl(self):
-        website = self
-        crawled = []
-        to_be_crawled = [website]
-        while len(crawled) < 10:
-            start = pages.Page(to_be_crawled[0])
-            start.load()
-            to_be_crawled += start.links
-            to_be_crawled = list(dict.fromkeys(to_be_crawled))
-            crawled.append(start)
-            to_be_crawled.pop(0)
-        return (crawled)
+
+def crawl(self):
+    website = self
+    crawled = []
+    to_be_crawled = [website]
+    while len(crawled) < 10:
+        start = pages.Page(to_be_crawled[0])
+        start.load()
+        to_be_crawled += start.links
+        to_be_crawled = list(dict.fromkeys(to_be_crawled))
+        crawled.append(start)
+        to_be_crawled.pop(0)
+    return (crawled)
