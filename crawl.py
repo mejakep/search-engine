b5715e9a583aa52c60f6173aeb05e51c2d463e2c
Diacryptic <Diacryptic@users.noreply.github.com>
2020-03-02 15:13:15 +0000

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,14 +1,17 @@
-import pages
-from random import randint
+import pages #imports the pages module
+from random import randint #imports the randint module for rng
 
-def crawl(website, depth = 10):
-    crawled = []
-    to_be_crawled = [website]
+def crawl(website, depth = 10): #website = the link you want to start the crawl from, depth = how many pages you want crawl
+    crawled = [] #a list which holds the links the crawl function has already visited
+    crawled_urls = []
+    to_be_crawled = [website] #a list which holds the links that have not yet been explored from the sites that have been crawled
     while len(crawled) < depth:
-        x = randint(0, (len(to_be_crawled)-1))
-        start = pages.Page(to_be_crawled.pop(x))
-        start.load()
-        to_be_crawled += start.links
-        to_be_crawled = list(dict.fromkeys(to_be_crawled))
-        crawled.append(start)
+        x = randint(0, (len(to_be_crawled)-1)) #rng for fair distribution of crawling
+        if not to_be_crawled[x] in crawled_urls:
+            start = pages.Page(to_be_crawled.pop(x)) #calls the Page function to turn the link selected by the rng in the to_be_crawled list into an object that can have its HTML extracted (see pages.py)
+            crawled_urls.append(start.url) 
+            start.load() #calls the load.() function to extect the HTML and links on that page
+            to_be_crawled += start.links #adds the links found on the current page to the to_be_crawled list for future crawling
+            to_be_crawled = list(dict.fromkeys(to_be_crawled)) # removes dupes in to_be_crawled
+            crawled.append(start) #after all links have been explored, the current site is added to the 
     return (crawled)
