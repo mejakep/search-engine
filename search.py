29f36c5d78c1bb2baaa4c4ea206436fa8b5ad79c
Diacryptic <Diacryptic@users.noreply.github.com>
2020-03-04 18:53:42 +0000

    Update search.py
    created a search algorithm, need to sort the result term frequencies however

--- search.py
+++ search.py
@@ -1,11 +1,19 @@
 import crawl
-website = crawl.crawl("https://www.whsb.essex.sch.uk", 10)
-result = []
+website = crawl.crawl("https://www.wikipedia.org/", 100)
 
 def search(term):
-    for page in website:
-        if (term) in page.term_frequency:
-            result = page.term_frequency[term]
-            return (result)
+    x = 0
+    result = []
+    while x != (len(website)-1):
+        if term in website[x].term_frequency:
+            result.append(website[x])
+            x += 1
+        else:
+            x += 1
+    return (result)
+    #for page in website:
+        #if (term) in page.term_frequency:
+            #result = page.term_frequency[term]
+            #return (result)
 
 search("the")
