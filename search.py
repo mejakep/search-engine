59e788694dbc168b46419291a4013b74cf0bd03f
Diacryptic <Diacryptic@users.noreply.github.com>
2020-03-05 08:33:15 +0000

    Update search.py

--- search.py
+++ search.py
@@ -1,19 +1,15 @@
 import crawl
-website = crawl.crawl("https://www.wikipedia.org/", 100)
+website = crawl.crawl("https://www.wikipedia.org/", 50)
+searched = []
 
 def search(term):
     x = 0
-    result = []
+    searched = []
     while x != (len(website)-1):
         if term in website[x].term_frequency:
-            result.append(website[x])
+            searched.append(website[x])
             x += 1
         else:
             x += 1
-    return (result)
-    #for page in website:
-        #if (term) in page.term_frequency:
-            #result = page.term_frequency[term]
-            #return (result)
-
-search("the")
+    searched.sort(key=lambda x: x.term_frequency[term], reverse=True)
+    return (searched)
